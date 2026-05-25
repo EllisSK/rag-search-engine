1abb384c6ce697f1d89b50c979d63409a515ddad
EllisSK <ellissinclairkent@gmail.com>
2026-05-25 15:55:37 +0100

    CH1L4

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
+
+from pathlib import Path
+
+def search(query: str) -> list[dict]:
+    movie_path = "data/movies.json"
+
+    with open(movie_path, "r") as f:
+        movie_data = json.load(f)
+
+    matched = []
+
+    for movie in movie_data["movies"]:
+        if query in movie["title"]:
+            matched.append(movie)
+
+    return matched
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -14,6 +31,17 @@ def main() -> None:
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
+            results = search(args.query)
+            num = 1
+
+            if results:
+                for movie in results:
+                    print(f"{num}. {movie["title"]}")
+                    num += 1
+                    if num >= 6:
+                        break
+            else:
+                print("No results found")
         case _:
             parser.print_help()
 
