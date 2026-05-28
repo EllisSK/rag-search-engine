24efd2bfb55129d410841a598b5f1b31848c4b58
Ellis Sinclair-Kent <ellissinclairkent@gmail.com>
2026-05-28 10:05:51 +0100

    CH4L10

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -7,7 +7,7 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 import argparse
 
-from search_core.semantic_search import verify_model
+from search_core.semantic_search import verify_model, embed_text
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -15,11 +15,17 @@ def main() -> None:
 
     verify_parser = subparsers.add_parser("verify", help="Verify model info")
 
+    embedding_parser = subparsers.add_parser("embed_text", help="Embed some text")
+    embedding_parser.add_argument("text", type=str, help="Text to embed")
+
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+
+        case "embed_text":
+            embed_text(args.text)
 
         case _:
             parser.print_help()
--- search_core/semantic_search.py
+++ search_core/semantic_search.py
@@ -4,9 +4,25 @@ class SemanticSearch:
     def __init__(self) -> None:
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
 
+    def generate_embedding(self, text: str):
+        if not text.strip():
+            raise ValueError("Bad text")
+        
+        embedding = self.model.encode([text])
+
+        return embedding[0]
+
 def verify_model():
 
     ss = SemanticSearch()
 
     print(f"Model loaded: {ss.model}")
     print(f"Max sequence length: {ss.model.max_seq_length}")
+
+def embed_text(text: str):
+    ss = SemanticSearch()
+    embedding = ss.generate_embedding(text)
+
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
